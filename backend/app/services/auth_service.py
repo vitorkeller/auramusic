763106228dbd029f3c674557e1dac42200b42d09
vitorkeller <vitorkeller1604@hotmail.com>
2026-05-18 19:10:27 -0300

    fix: corrige carregamento do dotenv e variáveis JWT

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -3,10 +3,13 @@ from passlib.context import CryptContext
 from jose import jwt
 from datetime import datetime, timedelta, timezone
 import os
+from dotenv import load_dotenv
 from app.repositories.user_repository import (
     get_user_by_username,
     create_user
 )
+
+load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
